69a99c120cdf55e3a04d6a642d77b2fc0830a922
Robbie Muchiri <developedbyrobbie@gmail.com>
2024-01-29 18:21:34 +0300

    database setup

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const teamRoutes = require('./routes/team');
+const { Pool } = require('pg');
 const pg = require('pg');
 // const { sequelize } = require('./models');
 // const config = require('./config.json');
@@ -30,20 +31,8 @@ app.use('/team', teamRoutes);
 /*  DATABASE SETUP */
 require('dotenv').config();
 
-const conString = process.env.DB_URL;
-const client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].theTime);
-    // >> output: 2018-08-23T14:02:57.117Z
-    client.end();
-  });
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
 });
 
 
