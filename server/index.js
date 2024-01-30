7a2d5b7a75906f175429a8d6249cc46d4a2fe480
Robbie Muchiri <developedbyrobbie@gmail.com>
2024-01-30 12:06:24 +0300

    database setup

--- server/index.js
+++ server/index.js
@@ -29,12 +29,29 @@ app.use('/team', teamRoutes);
 
 
 /*  DATABASE SETUP */
+const postgres = require('postgres');
 require('dotenv').config();
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
 });
 
+async function getPgVersion() {
+  const result = await sql`select version()`;
+  console.log(result);
+}
+
+getPgVersion();
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
